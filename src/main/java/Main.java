b8ede480d6479148a2624b8a19948c5d028141bd
Sandeep <skumar@gainsight.com>
2015-05-13 18:01:30 +0530

    Added proc file for windows.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -61,7 +61,8 @@ public class Main extends HttpServlet {
   }
 
   public static void main(String[] args) throws Exception {
-    Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+    System.out.println("Inside Java Main.");
+	Server server = new Server(Integer.valueOf(System.getenv("PORT")));
     ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
     context.setContextPath("/");
     server.setHandler(context);
