d354dc95a87ad9a985312944ac26a0713ba9a9c2
Sandeep <skumar@gainsight.com>
2015-05-13 19:13:34 +0530

    Image reading

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,16 @@ import org.eclipse.jetty.servlet.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 
 public class Main extends HttpServlet {
+	String imageUrl = "http://www.avajava.com/images/avajavalogo.jpg";
+	String destinationFile = "D:/Workspace/JavaProject/appimage.jpg";
+	
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
@@ -15,7 +23,7 @@ public class Main extends HttpServlet {
     if (req.getRequestURI().endsWith("/db")) {
       showDatabase(req,resp);
     } else {
-      showHome(req,resp);
+      saveImage(imageUrl, destinationFile);
     }
   }
 
@@ -70,4 +78,21 @@ public class Main extends HttpServlet {
     server.start();
     server.join();
   }
+  
+  public  void saveImage(String imageUrl, String destinationFile) throws IOException {
+		URL url = new URL(imageUrl);
+		InputStream is = url.openStream();
+		OutputStream os = new FileOutputStream(destinationFile);
+
+		byte[] b = new byte[2048];
+		int length;
+
+		while ((length = is.read(b)) != -1) {
+			os.write(b, 0, length);
+		}
+
+		is.close();
+		os.close();
+	}
+
 }
